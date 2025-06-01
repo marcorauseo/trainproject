90d0096d0f6f0eed61d410875d66376a9271b88a
Marco Rauseo <rauseom@libero.it>
2025-06-01 17:09:09 +0200

    connessione db ok

--- backend/app.js
+++ backend/app.js
@@ -11,3 +11,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server in ascolto sulla porta ${PORT}`);
 });
+
+app.get('/', (req, res) => {
+  res.send('Benvenuto in TreniApp!');
+});
